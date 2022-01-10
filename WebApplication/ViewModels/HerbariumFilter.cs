06f5985fc42fca018f9591285b2280b2757cbc20
PatrykGrzegorek <patrykgrzegorekjob@gmail.com>
2022-01-10 05:05:08 +0100

    save

--- WebApplication/ViewModels/HerbariumFilter.cs
+++ WebApplication/ViewModels/HerbariumFilter.cs
@@ -1,68 +1,68 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Globalization;
-using WebApplication.Models;
+﻿//using System;
+//using System.ComponentModel.DataAnnotations;
+//using System.Linq;
+//using System.Globalization;
+//using WebApplication.Models;
 
-namespace WebApplication.ViewModels
-{
-    public class HerbariumFilter: IPageFilter
-    {
-        public int? CollectionId { get; set; }
-        public string CollectionName { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime? DateFrom { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime? DateTo { get; set; }
+//namespace WebApplication.ViewModels
+//{
+//    public class HerbariumFilter: IPageFilter
+//    {
+//        public int? CollectionId { get; set; }
+//        public string CollectionName { get; set; }
+//        [DataType(DataType.Date)]
+//        public DateTime? DateFrom { get; set; }
+//        [DataType(DataType.Date)]
+//        public DateTime? DateTo { get; set; }
 
-        public bool IsEmpty()
-        {
-            bool active = CollectionId.HasValue
-                          || DateFrom.HasValue
-                          || DateTo.HasValue;
-            return !active;
-        }
+//        public bool IsEmpty()
+//        {
+//            bool active = CollectionId.HasValue
+//                          || DateFrom.HasValue
+//                          || DateTo.HasValue;
+//            return !active;
+//        }
 
-        public override string ToString()
-        {
-            return string.Format("{0}-{1}-{2}-{3}-{4}",
-                CollectionId,
-                DateFrom?.ToString("dd.MM.yyyy"),
-                DateTo?.ToString("dd.MM.yyyy"));
-        }
+//        public override string ToString()
+//        {
+//            return string.Format("{0}-{1}-{2}-{3}-{4}",
+//                CollectionId,
+//                DateFrom?.ToString("dd.MM.yyyy"),
+//                DateTo?.ToString("dd.MM.yyyy"));
+//        }
 
-        public static HerbariumFilter FromString(string s)
-        {
-            var filter = new HerbariumFilter();
-            if (!string.IsNullOrEmpty(s))
-            {
-                string[] arr = s.Split('-', StringSplitOptions.None);
+//        public static HerbariumFilter FromString(string s)
+//        {
+//            var filter = new HerbariumFilter();
+//            if (!string.IsNullOrEmpty(s))
+//            {
+//                string[] arr = s.Split('-', StringSplitOptions.None);
 
-                if (arr.Length == 5)
-                {
-                    filter.CollectionId = string.IsNullOrWhiteSpace(arr[0]) ? new int?() : int.Parse(arr[0]);
-                    filter.DateFrom = string.IsNullOrWhiteSpace(arr[1]) ? new DateTime?() : DateTime.ParseExact(arr[1], "dd.MM.yyyy", CultureInfo.InvariantCulture);
-                    filter.DateTo = string.IsNullOrWhiteSpace(arr[2]) ? new DateTime?() : DateTime.ParseExact(arr[2], "dd.MM.yyyy", CultureInfo.InvariantCulture);
-                }
-            }
-            return filter;
-        }
+//                if (arr.Length == 5)
+//                {
+//                    filter.CollectionId = string.IsNullOrWhiteSpace(arr[0]) ? new int?() : int.Parse(arr[0]);
+//                    filter.DateFrom = string.IsNullOrWhiteSpace(arr[1]) ? new DateTime?() : DateTime.ParseExact(arr[1], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+//                    filter.DateTo = string.IsNullOrWhiteSpace(arr[2]) ? new DateTime?() : DateTime.ParseExact(arr[2], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+//                }
+//            }
+//            return filter;
+//        }
 
-        public IQueryable<ViewHerbariumInfo> Apply(IQueryable<ViewHerbariumInfo> query)
-        {
-            if (CollectionId.HasValue)
-            {
-                query = query.Where(d => d.CollectionId == CollectionId.Value);
-            }
-            if (DateFrom.HasValue)
-            {
-                query = query.Where(d => d.YearOfCollection >= DateFrom.Value);
-            }
-            if (DateFrom.HasValue)
-            {
-                query = query.Where(d => d.YearOfCollection <= DateFrom.Value);
-            }
-            return query;
-        }
-    }
-}
+//        public IQueryable<ViewHerbariumInfo> Apply(IQueryable<ViewHerbariumInfo> query)
+//        {
+//            if (CollectionId.HasValue)
+//            {
+//                query = query.Where(d => d.CollectionId == CollectionId.Value);
+//            }
+//            if (DateFrom.HasValue)
+//            {
+//                query = query.Where(d => d.YearOfCollection >= DateFrom.Value);
+//            }
+//            if (DateFrom.HasValue)
+//            {
+//                query = query.Where(d => d.YearOfCollection <= DateFrom.Value);
+//            }
+//            return query;
+//        }
+//    }
+//}
